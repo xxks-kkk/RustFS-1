92586846f669b2a04651bdad99166e54f93e36f4
xxks-kkk <ferrishu3886@gmail.com>
2018-09-29 14:50:55 -0500

    Add comments

--- src/directory.rs
+++ src/directory.rs
@@ -1,10 +1,17 @@
 use file::File;
 use file::File::Directory;
 
+// Traits are similar to a feature often called interfaces in other languages,
+// although with some differences.
+// Sized: https://doc.rust-lang.org/std/marker/trait.Sized.html
 pub trait DirectoryHandle<'r>: Sized {
+  // The Self keyword is an alias for the type we?re implementing the traits or methods on.
   fn is_dir(&self) -> bool;
   fn insert(&mut self, name: &'r str, file: Self);
   fn remove(&mut self, name: &'r str);
+  // return Option<Self> idnicates that whatever type implements the trait,
+  // we will return the type wrapped as an option
+  // https://doc.rust-lang.org/book/2018-edition/ch17-02-trait-objects.html
   fn get(&self, name: &'r str) -> Option<Self>;
 }
 
